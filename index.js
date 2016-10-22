f43ba8d8548f6cde3aeb8068555c9fb7cfe4a284
Jonathan Sandgren <jonathansand@live.se>
2016-10-23 00:19:31 +0200

    fixes and procfile

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res){
     var obj = {ipaddress : req.connection.remoteAddress,
-              language : req.get('Accept-Language'),
-              software : req.get('User-Agent')};
+              language : req.get('Accept-Language').split(",")[0],
+              software : req.get('User-Agent').split("(")[1].split(")")[0]};
     res.end(JSON.stringify(obj));
 });
 
