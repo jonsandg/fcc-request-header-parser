4d7919f869fd0d186b86855d206b190fab8b4f38
Jonathan Sandgren <jonathansand@live.se>
2016-10-23 00:23:36 +0200

    ip fix

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res){
-    var obj = {ipaddress : req.connection.remoteAddress,
+    var obj = {ipaddress : req.headers['x-forwarded-for'] || 
+                            req.connection.remoteAddress || 
+                            req.socket.remoteAddress ||
+                            req.connection.socket.remoteAddress,
               language : req.get('Accept-Language').split(",")[0],
               software : req.get('User-Agent').split("(")[1].split(")")[0]};
     res.end(JSON.stringify(obj));
